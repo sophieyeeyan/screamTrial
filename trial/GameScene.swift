2ea006a9954c072809dee7e4d7d63a8cf2edb08e
sophieyeeyan <liuyy124@gmail.com>
2021-03-16 22:42:12 +0800

    Update GameScene.swift

--- trial/GameScene.swift
+++ trial/GameScene.swift
@@ -14,7 +14,7 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-        
+        /*
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
@@ -35,39 +35,48 @@ class GameScene: SKScene {
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
         }
+ */
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
+        /*
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.green
             self.addChild(n)
         }
+ */
     }
     
     func touchMoved(toPoint pos : CGPoint) {
+        /*
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.blue
             self.addChild(n)
         }
+        */
     }
     
     func touchUp(atPoint pos : CGPoint) {
+        /*
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.red
             self.addChild(n)
         }
+        */
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        /*
         if let label = self.label {
             label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
         }
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        */
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
