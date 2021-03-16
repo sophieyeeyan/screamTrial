bf9dbeace21b575b6191b2335aed85a5bd598ecb
sophieyeeyan <liuyy124@gmail.com>
2021-03-16 19:42:02 +0800

    Update GameViewController.swift

--- trial/GameViewController.swift
+++ trial/GameViewController.swift
@@ -11,8 +11,30 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var ImageViewTwo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let imagesNames = ["bol_1","bol_2", "bol_3","bol_4","bol_5", "bol_6","bol_7","bol_8","bol_9","bol_10"]
+        
+        let imageNamesTwo = ["roar_1", "roar_2"]
+        
+        var images = [UIImage]()
+        for i in 0..<imagesNames.count{
+            print("trying to load imageNames" + imagesNames [i] )
+            images.append(UIImage(named: imagesNames[i])!)
+        }
+        
+        ImageView.animationImages = images
+        ImageView.animationDuration = 5.0
+    
+        func touchDown(atPoint pos : CGPoint) {
+            ImageView.startAnimating()
+
+            }
+        
+
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
