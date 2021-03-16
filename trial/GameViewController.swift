af87646e305fe16112212d62f7cd388690f12089
sophieyeeyan <liuyy124@gmail.com>
2021-03-16 22:42:10 +0800

    Update GameViewController.swift

--- trial/GameViewController.swift
+++ trial/GameViewController.swift
@@ -11,31 +11,29 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    @IBOutlet weak var ImageView: UIImageView!
-    @IBOutlet weak var ImageViewTwo: UIImageView!
+    @IBOutlet weak var animation1: UIImageView!
+    @IBOutlet weak var animation2: UIImageView!
+    
+    var tap: UITapGestureRecognizer!
+        
+    @objc func handleTap(_ gesture: UITapGestureRecognizer) {
+        touchDown(view2play: animation1)
+        touchDown(view2play: animation2)
+
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tap = UITapGestureRecognizer(target: self, action: #selector(GameViewController.handleTap(_:)))
+        view.addGestureRecognizer(tap)
         
-        let imagesNames = ["bol_1","bol_2", "bol_3","bol_4","bol_5", "bol_6","bol_7","bol_8","bol_9","bol_10"]
+        let destoryImages = ["bol_1","bol_2", "bol_3","bol_4","bol_5", "bol_6","bol_7","bol_8","bol_9","bol_10"]
         
-        let imageNamesTwo = ["roar_1", "roar_2"]
-        
-        var images = [UIImage]()
-        for i in 0..<imagesNames.count{
-            print("trying to load imageNames" + imagesNames [i] )
-            images.append(UIImage(named: imagesNames[i])!)
-        }
-        
-        ImageView.animationImages = images
-        ImageView.animationDuration = 5.0
-    
-        func touchDown(atPoint pos : CGPoint) {
-            ImageView.startAnimating()
+        let dinoImages = ["roar_1", "roar_2"]
+       
+        loadAnimation(view2load: self.animation1, imageNames: destoryImages, animationDuration: 5.0)
+        loadAnimation(view2load: self.animation2, imageNames: dinoImages, animationDuration: 1.0)
 
-            }
-        
-
-        
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
@@ -51,6 +49,25 @@ class GameViewController: UIViewController {
             view.showsFPS = true
             view.showsNodeCount = true
         }
+    }
+    
+    
+    func loadAnimation (view2load: UIImageView, imageNames: [String], animationDuration: Double) {
+        var images = [UIImage]()
+        for i in 0..<imageNames.count{
+            print("trying to load imageNames" + imageNames [i] )
+            images.append(UIImage(named: imageNames[i])!)
+        }
+        
+        view2load.animationImages = images
+        view2load.animationDuration = animationDuration
+    }
+   
+
+    func touchDown(view2play: UIImageView) {
+        print("touched")
+        view2play.startAnimating()
+
     }
 
     override var shouldAutorotate: Bool {
